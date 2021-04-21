0abe5a8ee9590e9e3cf73e07ea8deefd385f3ccc
Gabriele <gabriele.iannola@gmail.com>
2021-04-21 15:16:24 +0200

    corrected navbar

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function test(){
+function apriForm(){
     let v = document.querySelector(".myFormDiv").style;
     if(v.display=="block") v.display = "none";
     else v.display = "block";
-    //document.querySelector(".myFormDiv").style.display = "block";
 }
