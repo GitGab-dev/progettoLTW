3d902aab81a8d781f919549e51d708613981ef47
Gabriele <gabriele.iannola@gmail.com>
2021-04-24 13:37:57 +0200

    added creaEvento,searchForm and logo. Improved forms

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function apriForm(){
-    let v = document.querySelector(".myFormDiv");
+function apriLogin(){
+    let v = document.getElementById("loginFormDiv");
     if(v.style.display=="block") v.style.display = "none";
     else {
+        let v2 = document.getElementById("searchFormDiv");
+        if(v2.style.display == "block"){
+            v2.style.display = "none";
+        }
         v.style.display = "block";
         let myForm = v.children[0];
         let storedEmail = localStorage.getItem("e");
@@ -13,8 +17,20 @@ function apriForm(){
     }
 }
 
+function apriSearch(){
+    let v = document.getElementById("searchFormDiv");
+    if(v.style.display=="block") v.style.display = "none";
+    else {
+        let v2 = document.getElementById("loginFormDiv");
+        if(v2.style.display == "block"){
+            v2.style.display = "none";
+        }
+        v.style.display = "block";
+    }
+}
+
 function controllaLogin(){
-    let myForm = document.querySelector(".myForm");
+    let myForm = document.getElementById("loginFormDiv").firstElementChild;
     let email = myForm.emailLogin.value;
     let pass = myForm.passLogin.value;
     if(!email){
@@ -29,6 +45,26 @@ function controllaLogin(){
     if(myForm.rememberBox.checked){
         localStorage.setItem("e", email);
         localStorage.setItem("p", pass);
+    }
+
+    return true;
+}
+
+function controllaSearch(){
+    let myForm = document.getElementById("searchFormDiv").firstElementChild;
+    let cat = myForm.cercaCategoria.value;
+    let dataInit = myForm.cercaDal.valueAsNumber;
+    let dataEnd = myForm.cercaAl.valueAsNumber;
+
+    if (cat == "default") {
+        alert("Scegli la categoria");
+        return false;
+    }
+
+
+    if(dataEnd - dataInit < 0) {
+        alert("Le date non sono accettabili");
+        return false;
     }
 
     return true;
